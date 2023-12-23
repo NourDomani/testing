197d8f05ee2870c596b1a1c208d653ce5ee827de
Nour Domani <138979883+NourDomani@users.noreply.github.com>
2023-12-23 15:57:40 +0300

    Create Area of a circle.cpp

---
+++ Area of a circle.cpp
@@ -0,0 +1,17 @@
+#include<iostream>
+using namespace std;
+int main()
+{
+	double R, area;
+	cout << "enter your Radious\n"<<"R=";
+	cin >> R;
+	area = 3.14 * (R * R);
+	cout << "The area is :" << area;
+	return 0;
+}
+
+
+
+
+
+
